f5ccd8101352d4fc2d54e29c894cf0c43d4a12d4
Muhammad Umer Farooq <mufy.aheer@gmail.com>
2021-01-17 12:15:10 +0500

    Add Swagger OAS

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './env.validation';
 
@@ -9,6 +10,15 @@ async function bootstrap() {
   const configService = app.get<ConfigService<EnvironmentVariables>>(
     ConfigService,
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest TypeScript Starter')
+    .setDescription('Nest TypeScript starter API docs')
+    .setVersion('1.0.0')
+    .addTag('app', 'Application global module')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
